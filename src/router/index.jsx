201b1d425ad9fbb3ce28ad7529e8f27e189da7e2
samar0405 <samariddinalmardanov1@gmail.com>
2024-06-15 03:07:58 +0500

    task7-2

---
+++ src/router/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "../App";
+import Login from "../components/login/login";
+import Tasks from "../components/tasks/tasks";
+import Main from "../components/main/main";
+import Products from "../components/products/products";
+import Users from "../components/users/index";
+
+const Index = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<App />}>
+        <Route index element={<Login />} />
+
+        <Route path="main" element={<Main />}>
+          <Route index element={<Tasks />} />
+          <Route path="products" element={<Products />} />
+          <Route path="users" element={<Users />} />
+        </Route>
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
+
+export default Index;
